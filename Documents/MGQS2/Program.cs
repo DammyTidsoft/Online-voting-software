3add28f374a4cd8f740655cc8b5cfb8d422df60c
horffshytu <shittuabdulazeezas@gmail.com>
2023-09-20 17:44:56 +0100

    my 1st ludo game app using if condition

---
+++ Documents/MGQS2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Examination calculator");
+Console.WriteLine("My First Calculator");
+Console.WriteLine("The name of the founder calculator is HORFFSHYTU");
+// Working with variables
+string name = "abdulazeez";
+float testscore = 28;
+float examscore = 58;
+double totalscore = testscore + examscore;
+
+Console.WriteLine("My name is" + "  " +name);
+Console.WriteLine("Your test score is" + " " +testscore);
+Console.WriteLine("Your exam score is" + " " +examscore);
+Console.WriteLine("Your total score is" + " " +totalscore);
